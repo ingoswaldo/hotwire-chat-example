e002a2092671b5f7aefb98f998c5601cb5802567
ingoswaldo <oswaldo.montess@cecar.edu.co>
2023-04-21 12:17:11 -0500

    Devise initial sets up

--- Gemfile
+++ Gemfile
@@ -30,9 +30,6 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 gem "redis", "~> 4.0"
 
-# Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
-# gem "kredis"
-
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem "bcrypt", "~> 3.1.7"
 
@@ -42,11 +39,11 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 
-# Use Sass to process CSS
-# gem "sassc-rails"
+# Use Devise to authentication flow
+gem 'devise', '~> 4.9', '>= 4.9.2'
 
-# Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+# User Responders to improve devise and Hotwire / Turbo integration
+gem 'responders', '~> 3.1'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -56,12 +53,6 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-
-  # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
-  # gem "rack-mini-profiler"
-
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
 end
 
 group :test do
---
+++ config/initializers/devise.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+# Assuming you have not yet modified this file, each configuration option below
+# is set to its default value. Note that some are commented out while others
+# are not: uncommented lines are intended to protect your configuration from
+# breaking changes in upgrades (i.e., in the event that future versions of
+# Devise change the default values for those options).
+#
+# Use this hook to configure devise mailer, warden hooks and so forth.
+# Many of these configuration options can be set straight in your model.
+Devise.setup do |config|
+  # ==> Mailer Configuration
+  config.mailer_sender = 'devise@example.com'
+
+  # ==> ORM configuration
+  require 'devise/orm/active_record'
+
+  # ==> Configuration for any authentication mechanism
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+  config.skip_session_storage = [:http_auth]
+
+  # ==> Configuration for :database_authenticatable
+  config.stretches = Rails.env.test? ? 1 : 12
+
+  # ==> Configuration for :confirmable
+  config.reconfirmable = true
+
+  # ==> Configuration for :rememberable
+  config.expire_all_remember_me_on_sign_out = true
+
+  # ==> Configuration for :validatable
+  config.password_length = 6..128
+  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+
+  # ==> Configuration for :recoverable
+  config.reset_password_within = 6.hours
+
+  # ==> Navigation configuration
+  config.sign_out_via = :delete
+
+  # ==> Hotwire/Turbo configuration
+  config.responder.error_status = :unprocessable_entity
+  config.responder.redirect_status = :see_other
+end
