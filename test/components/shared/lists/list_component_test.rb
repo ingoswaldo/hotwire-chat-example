0e9006eb47c66e1569dcfc214f566c61e080302a
ingoswaldo <oswaldo.montess@cecar.edu.co>
2023-04-25 11:25:35 -0500

    Add component tests

--- test/components/shared/lists/list_component_test.rb
+++ test/components/shared/lists/list_component_test.rb
@@ -5,11 +5,17 @@ require 'test_helper'
 module Shared
   module Lists
     class ListComponentTest < ViewComponent::TestCase
-      def test_component_renders_something_useful
-        # assert_equal(
-        #   %(<span>Hello, components!</span>),
-        #   render_inline(Shared::ListComponent.new(message: "Hello, components!")).css("span").to_html
-        # )
+      def test_render_component
+        list_component = ListComponent.new.tap do |list|
+          list.with_item(text: 'Apple', negative_check: false)
+          list.with_item(text: 'Orange', negative_check: false)
+        end
+
+        render_inline(list_component)
+
+        assert_selector('ul')
+        assert_selector('li', text: 'Apple')
+        assert_selector('li', text: 'Orange')
       end
     end
   end
--- test/components/shared/lists/list_item_component_test.rb
+++ test/components/shared/lists/list_item_component_test.rb
@@ -5,11 +5,10 @@ require 'test_helper'
 module Shared
   module Lists
     class ListItemComponentTest < ViewComponent::TestCase
-      def test_component_renders_something_useful
-        # assert_equal(
-        #   %(<span>Hello, components!</span>),
-        #   render_inline(Shared::ListItemComponent.new(message: "Hello, components!")).css("span").to_html
-        # )
+      def test_render_component
+        render_inline(ListItemComponent.new(text: 'My item', negative_check: false))
+
+        assert_text('My item')
       end
     end
   end
