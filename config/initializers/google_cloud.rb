3e331e493dc931ec502f267d1f0439b82967a018
Oswaldo Montes <ing.oswaldoms@gmail.com>
2023-08-15 12:41:00 -0500

    Disable load google credentials for testing environment

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Google::Cloud::Translate.configure do |config|
-  config.credentials = Rails.application.credentials.google.credentials
+  config.credentials = Rails.application.credentials.google.credentials unless Rails.env.test?
 end
